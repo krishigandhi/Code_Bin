4c3cad9cc1a0b0d50850e400a41db6e2fd6e1ed9
Krishi Gandhi <krishigandhi2601@gmail.com>
2024-08-07 20:20:13 +0000

    Code Bin

---
+++ code-bin/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCgy9d6B8xcM103Ju3wRK07LICuCp5TgTI",
+    authDomain: "codebin-angular-d34e2.firebaseapp.com",
+    projectId: "codebin-angular-d34e2",
+    storageBucket: "codebin-angular-d34e2.appspot.com",
+    messagingSenderId: "1021467299185",
+    appId: "1:1021467299185:web:e96ceae8fae091e64b7ad0"
+  };
