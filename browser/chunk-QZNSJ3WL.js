d29bf9017cc26c34378cc0e4eb33928be520ea1e
krishigandhi <98482670+krishigandhi@users.noreply.github.com>
2024-08-08 08:39:59 +0000

    Auto-generated commit

---
+++ browser/chunk-QZNSJ3WL.js
@@ -0,0 +1,2 @@
+import{$a as r,Oa as o,Pa as a,X as n,Ya as i}from"./chunk-2QWDLPGD.js";var c=(()=>{class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=n({type:e,selectors:[["app-heavy"]],standalone:!0,features:[r],decls:2,vars:0,template:function(t,m){t&1&&(o(0,"h1"),i(1,` This is heavy component
+`),a())}})}return e})();export{c as HeavyComponent};
